3f4a0278cc20679f738ab5165d2df3a72e197e5f
nilia2020 <jorge.niglia@gmail.com>
2021-08-14 20:58:49 -0300

    css nano y purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      require('tailwindcss'),
-    ]
-  }
+ plugins: [
+ require('tailwindcss'),
+ require('autoprefixer'),
+ purgecss({
+ content: [
+ './**/*.html',
+ //Para agregar soporte para otro tipo de archivos.
+ // './**/*.js',
+ // './**/*.vue'
+ ],
+ //IMPORTANTE: Para soportar pseudo-clases
+ defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+ }),
+ require('cssnano')({
+ preset: 'default',
+ })
+ ]
+}
